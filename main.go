def40046edec0482a93c21bc46a07e80eccb6e58
卓弘 <zhuohong@xmpaoyou.com>
2022-03-31 13:15:49 +0000

    new

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"os/user"
 
 	"github.com/diiyw/pp/builtin"
@@ -24,19 +23,11 @@ func init() {
 	}
 	handlerShell := ppDir + ".pp_profile"
 	_, err = os.Stat(handlerShell)
-	if err == nil {
-		return
-	}
-	err = os.WriteFile(handlerShell, []byte("#!/bin/zsh\n\ncommand_not_found_handler() {\n    pp $@\n    echo \"zsh: command not found: $1\"\n    return 127\n}"), 0755)
 	if err != nil {
-		log.Fatal("Initialize pp env failed: ", err)
-	}
-	cmd := exec.Command("source", handlerShell)
-	cmd.Stdout = nil
-	cmd.Stdin = nil
-	cmd.Stderr = nil
-	if err = cmd.Run(); err != nil {
-		log.Fatal("source failed: ", err)
+		err = os.WriteFile(handlerShell, []byte("#!/bin/zsh\n\ncommand_not_found_handler() {\n    pp $@\n    echo \"zsh: command not found: $1\"\n    return 127\n}"), 0755)
+		if err != nil {
+			log.Fatal("Initialize pp env failed: ", err)
+		}
 	}
 }
 
